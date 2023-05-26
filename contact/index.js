b97c6c73131233fc6526335cf84fe2818659d087
Météore <79107506+meteorelevrai@users.noreply.github.com>
2023-05-26 08:49:42 +0200

    Add files via upload

---
+++ contact/index.js
@@ -0,0 +1,8 @@
+function menu_open(){
+    document.getElementById("menu").setAttribute("class", "menu-open")
+    document.getElementById("menu_open").setAttribute("class", "ico-menu-open-hid")
+}
+function menu_close(){
+    document.getElementById("menu").setAttribute("class", "menu-close")
+    document.getElementById("menu_open").setAttribute("class", "ico-menu-open-visi")
+}
